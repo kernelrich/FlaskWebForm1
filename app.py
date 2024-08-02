21cf90ef0269a6e47c88f66ff9ec460ca221693b
kernelrich <16345819+kernelrich@users.noreply.github.com>
2024-08-02 10:58:51 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'your secret key'
+
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+						
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+		
+		
